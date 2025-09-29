79b8a6f713336141c227fb73afd8c6b8cccb9f1d
SurkovKostiantyn <kskrua@gmail.com>
2025-09-29 13:56:28 +0300

    +script

--- pages/home.php
+++ pages/home.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$title?></title>
+    <link rel="icon" type="image/x-icon" href="skfbskdfbksdfb">
+    <script src="./scripts/script.js"></script>
 </head>
 <body>
     <?php include_once 'pages/nav.php'?>
