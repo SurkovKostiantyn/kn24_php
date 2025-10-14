fe67effd96ac409fa80945744be46bb8748d7053
SurkovKostiantyn <kskrua@gmail.com>
2025-10-14 14:25:16 +0300

    асинхронність

--- pages/home.php
+++ pages/home.php
@@ -33,5 +33,51 @@
 
     <img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=https%3A%2F%2Fkn24-php.onrender.com" alt="QR kn24-php">
 
+    <div id="div" style="list-style:none; display:flex; gap:10px; flex-wrap:wrap;">
+    </div>
+    <button id="btn">Додати картинку</button>
+
+    <script>
+        const url = "https://picsum.photos/300/300";
+        const div = document.getElementById("div");
+        const btn = document.getElementById("btn");
+        
+        // отримати картинку асинхронно з сайту "https://picsum.photos/300/300"
+        const getPicFromAPI = async (obj) => {
+            // Цей блок намагається виконати код всередині, або показати помилку
+            try { 
+                // в змінну response запишеться відповідь віддаленого сервера
+                const response = await fetch(url);
+
+                console.log(response); // Подивіться в консоль, що там приходить
+                console.log(response.url); // нас цікавить саме url
+
+                if (!response.ok) {
+                    // якщо сервер відповів з кодом помилки (наприклад, 404, 500)
+                    // пишемо в консоль помилку
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                // встановлюємо отриману картинку
+                obj.src = response.url;
+            } catch (error) {
+                console.error('Помилка при завантаженні зображення:', error);
+            }
+        };
+
+        // функція що додає картинку
+        const addPic = () => {
+            let img = document.createElement('img');
+            div.appendChild(img);
+            img.src = './images/1.jpg';
+
+            // а тепер викликаємо нашу асинхронну функцію
+            getPicFromAPI(img);
+        }
+        
+        // обробник подій для кнопки 
+        btn.addEventListener( 'click', addPic);
+
+    </script>
+
 </body>
 </html>
