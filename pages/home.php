9353f00cbb5684db6b57c9ee003c10511f552978
SurkovKostiantyn <kskrua@gmail.com>
2025-10-14 14:55:48 +0300

    асинхронність

--- pages/home.php
+++ pages/home.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($title ?? 'Сторінка', ENT_QUOTES, 'UTF-8') ?></title>
     <link rel="stylesheet" href="./styles/style.css">
-    <script src="./scripts/script.js"></script>
+
     <!-- Favicon базовий -->
     <link rel="icon" href="./images/icons/favicon.ico" sizes="any">
 
@@ -37,47 +37,7 @@
     </div>
     <button id="btn">Додати картинку</button>
 
-    <script>
-        const url = "https://picsum.photos/300/300";
-        const div = document.getElementById("div");
-        const btn = document.getElementById("btn");
-        
-        // отримати картинку асинхронно з сайту "https://picsum.photos/300/300"
-        const getPicFromAPI = async (obj) => {
-            // Цей блок намагається виконати код всередині, або показати помилку
-            try { 
-                // в змінну response запишеться відповідь віддаленого сервера
-                const response = await fetch(url);
-
-                console.log(response); // Подивіться в консоль, що там приходить
-                console.log(response.url); // нас цікавить саме url
-
-                if (!response.ok) {
-                    // якщо сервер відповів з кодом помилки (наприклад, 404, 500)
-                    // пишемо в консоль помилку
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                // встановлюємо отриману картинку
-                obj.src = response.url;
-            } catch (error) {
-                console.error('Помилка при завантаженні зображення:', error);
-            }
-        };
-
-        // функція що додає картинку
-        const addPic = () => {
-            let img = document.createElement('img');
-            div.appendChild(img);
-            img.src = './images/1.jpg';
-
-            // а тепер викликаємо нашу асинхронну функцію
-            getPicFromAPI(img);
-        }
-        
-        // обробник подій для кнопки 
-        btn.addEventListener( 'click', addPic);
-
-    </script>
+    <script src="./scripts/scriptAsync.js" defer></script>
 
 </body>
 </html>
