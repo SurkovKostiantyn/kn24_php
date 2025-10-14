070a5361eb68d5c58f60ff1410c941f24d3450ec
SurkovKostiantyn <kskrua@gmail.com>
2025-10-14 14:56:16 +0300

    асинхронність

--- pages/500.php
+++ pages/500.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($title ?? 'Сторінка', ENT_QUOTES, 'UTF-8') ?></title>
     <link rel="stylesheet" href="./styles/style.css">
-    <script src="./scripts/script.js"></script>
+
     <!-- Favicon базовий -->
     <link rel="icon" href="./images/icons/favicon.ico" sizes="any">
 
